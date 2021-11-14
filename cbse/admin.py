b8d9078010293c4a590e46f31f577a4409363a55
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-11-15 00:54:05 +0530

    added chapter and subject

--- cbse/admin.py
+++ cbse/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.db.models.fields import CharField
 
 from .models import *
 
 admin.site.register(Board)
 admin.site.register(Standard)
+admin.site.register(Subject)
+admin.site.register(Chapter)
--- cbse/models.py
+++ cbse/models.py
@@ -14,3 +14,10 @@ class Standard(models.Model):
     def __str__(self) -> str:
         return "{} -  {}".format(self.name, self.is_locked)
 
+class Subject(models.Model):
+    name = models.CharField(max_length=20, blank=False, null=False)
+
+class Chapter(models.Model):
+    name = models.CharField(max_length=30, null=False, blank=False)
+    chapter_link = models.URLField()
+    subject = models.ForeignKey(to=Subject, on_delete=models.CASCADE)
