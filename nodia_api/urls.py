7ca7570d161eedd386bc4cf5abd39b486993604f
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-11-15 00:36:54 +0530

    added urls

--- nodia_api/urls.py
+++ nodia_api/urls.py
@@ -4,5 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('generate_otp', generate_otp, name = 'generate_otp'),
-    path('verify_otp', verify_otp, name = 'verify_otp')
+    path('verify_otp', verify_otp, name = 'verify_otp'),
+    path('get_all_boards', get_all_boards, name = 'get_all_boards'),
+    path('get_all_standards', get_all_standards, name = 'get_all_standards')
 ]
