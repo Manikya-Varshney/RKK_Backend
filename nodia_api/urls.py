17bae07bf935ad9b571e884296f77ce9401c7ddb
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-11-14 17:31:48 +0530

    minor change

--- nodia_api/urls.py
+++ nodia_api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('generate_otp', generate_otp, name = 'generate_otp')
+    path('generate_otp', generate_otp, name = 'generate_otp'),
+    path('verify_otp', verify_otp, name = 'verify_otp')
 ]
