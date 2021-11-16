85ae1fe60b3ab1aba3f5a057435ee553325a64d2
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-11-17 00:17:26 +0530

    added plan model

--- users/models.py
+++ users/models.py
@@ -38,3 +38,9 @@ class Profile(models.Model):
 
     class Meta:
         verbose_name_plural = "Profiles"
+
+class Plan(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+    discount = models.IntegerField()
+    number_of_days = models.IntegerField()
