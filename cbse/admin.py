3bac500371168eb9615e677767988ff22bec200b
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-12-01 00:19:40 +0530

    minor changes for rank

--- cbse/admin.py
+++ cbse/admin.py
@@ -3,9 +3,13 @@ from django.db.models.fields import CharField
 
 from .models import *
 
+class ChapterDocumentAdmin(admin.ModelAdmin):
+    ordering = ('rank', )
+
+
 admin.site.register(Board)
 admin.site.register(Standard)
 admin.site.register(Subject)
 admin.site.register(Chapter)
-admin.site.register(ChapterDocument)
+admin.site.register(ChapterDocument, ChapterDocumentAdmin)
 admin.site.register(Language)
