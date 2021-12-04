f01b75fa8b848e91e937400d71404323883e6f35
Manikya-Varshney <manikya.varshney@gmail.com>
2021-12-04 17:01:25 +0530

    deploy

--- manage.py
+++ manage.py
@@ -2,9 +2,17 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import dotenv
+    
 
 def main():
+    """Run administrative tasks."""
+
+    # dotenv settings
+    dotenv.load_dotenv(
+        os.path.join(os.path.dirname(__file__), '.env')
+    )
+
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nodia.settings')
     try:
